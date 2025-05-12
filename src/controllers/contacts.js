488d484b9e8564b6b92c4213d4cf762717a72660
frirw <jasperbmg@gmail.com>
2025-05-12 10:28:51 +0300

    patchContactController upd

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -107,7 +107,7 @@ export const patchContactController = async (req, res) => {
   res.json({
     status: 200,
     message: 'Successfully patched a contact!',
-    data: result.contact,
+    data: result,
   });
 };
 
